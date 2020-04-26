970bb15f55009a74b925cfab8eb80a2a9f2286f0
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-04-26 17:15:00 +0900

    Enable redux on GoogleAuth component

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -3,7 +3,6 @@ import {connect} from "react-redux";
 import {signIn, signOut} from "../actions";
 
 class GoogleAuth extends React.Component {
-    state = { isSignedIn: null };
 
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
@@ -12,7 +11,8 @@ class GoogleAuth extends React.Component {
                 scope: 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({isSignedIn: this.auth.isSignedIn.get()});
+
+                this.onAuthChange(this.auth.isSignedIn.get());
                 this.auth.isSignedIn.listen(this.onAuthChange);
             })
         })
@@ -36,9 +36,9 @@ class GoogleAuth extends React.Component {
     };
 
     renderAuthButton() {
-        if (this.state.isSignedIn === null) {
+        if (this.props.isSignedIn === null) {
             return null;
-        } else if (this.state.isSignedIn) {
+        } else if (this.props.isSignedIn) {
             return (
                 <button onClick={this.onSignOut} className="ui red google button"><i className="google icon"></i>Sign Out</button>
             )
@@ -54,4 +54,8 @@ class GoogleAuth extends React.Component {
     }
 }
 
-export default connect(null, {signIn, signOut})(GoogleAuth);
+const mapStateToProps = (state) => {
+    return { isSignedIn: state.auth.isSignedIn };
+};
+
+export default connect(mapStateToProps, {signIn, signOut})(GoogleAuth);
